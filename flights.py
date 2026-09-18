7d4ab19e15d79aa6f5e06c5fca2c08bcbd90ac5e
Prof. Seo <hvyrain@ysc.ac.kr>
2026-09-18 09:16:17 +0000

    상관 계수 계산 방식을 개선하고 관련 주석 추가

--- flights.py
+++ flights.py
@@ -8,8 +8,12 @@ print(flt_year)
 
 sy = flt_year["year"].std()
 sp = flt_year["passengers"].std()
-cov = flt_year["year"].cov(flt_year["passengers"])
-print(f'r : {cov/(sy*sp)}')
+# cov = flt_year["year"].cov(flt_year["passengers"]) # 기존 라인 주석 처리
+# print(f'r : {cov/(sy*sp)}') # 기존 라인 주석 처리
+
+# 개선된 상관 계수 계산
+correlation = flt_year["year"].corr(flt_year["passengers"])
+print(f'r : {correlation:.3f}')
 sns.barplot(data=flt_year, x="year", y="passengers")
 plt.savefig("flights_barplot.png")
 
