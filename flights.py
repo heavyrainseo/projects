562f10423ff0ed16e4ed4089a4b5291f77e3ade0
Prof. Seo <hvyrain@ysc.ac.kr>
2026-09-18 08:48:23 +0000

    Refactor code structure for improved readability and maintainability

--- flights.py
+++ flights.py
@@ -3,8 +3,18 @@ import matplotlib.pyplot as plt
 
 flt = sns.load_dataset("flights")
 sns.lineplot(data=flt, x="year", y="passengers", hue="month")
-plt.show()
+plt.savefig("flights_lineplot.png")
 
 flt_year = flt.groupby("year", as_index=False)["passengers"].sum()
+print(flt_year)
+
+sy = flt_year["year"].std()
+sp = flt_year["passengers"].std()
+cov = flt_year["year"].cov(flt_year["passengers"])
+print(f'r : {cov/(sy*sp)}')
 sns.barplot(data=flt_year, x="year", y="passengers")
-plt.show() 
+plt.savefig("flights_barplot.png")
+
+flt_pivot = flt.pivot(index="month", columns="year", values="passengers")
+sns.heatmap(flt_pivot, annot=True, fmt="d", cmap="YlGnBu")
+plt.savefig("flights_heatmap.png")
